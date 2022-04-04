3c7473583947f9c8446b23110e5cdd1a6197fe25
Russel Hussain <md.russel.hussain@gmail.com>
2022-04-04 17:13:27 +0600

    Manage area done

---
+++ backend/app/routes/area.routes.js
@@ -0,0 +1,12 @@
+module.exports = app => {
+    const { getAreas, getArea, createArea, updateArea , deleteArea}    = require("../controllers/area.controller");
+    const router            = require("express").Router();
+
+    router.get("/", getAreas);
+    router.get("/:id", getArea);
+    router.post("/", createArea);
+    router.put('/:id', updateArea);
+    router.delete('/:id', deleteArea);
+
+    app.use('/api/v0/area', router);
+}
--- backend/app/routes/index.js
+++ backend/app/routes/index.js
@@ -1,5 +1,4 @@
 module.exports = app => {
     require("../routes/auth.routes.js") (app);
-    //require("../routes/user.routes.js") (app);
-    //require("../routes/quote.routes.js") (app);
+    require("../routes/area.routes.js") (app);
 }
