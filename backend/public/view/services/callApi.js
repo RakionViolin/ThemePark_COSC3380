6e88a8400892dc5986d8250bbdd8c2847ca27073
Russel Hussain <md.russel.hussain@gmail.com>
2022-04-05 16:35:15 +0600

    Dashboard ongoing

---
+++ backend/public/view/services/callApi.js
@@ -0,0 +1,23 @@
+
+export default  {
+    headers: () => {
+        let token =  localStorage.getItem("accessToken");
+
+        if (token) {
+            return {
+                "Content-type": "application/json;charset=UTF-8",
+                "Access-Control-Allow-Origin": "http://localhost:8080/*",
+                'x-access-token': token,
+                'Authorization' : 'Bearer '+token
+            };
+        } else {
+            return {
+                "Content-type": "application/json;charset=UTF-8",
+                "Access-Control-Allow-Origin": "http://localhost:8080/*",
+            };
+        }
+    },
+    login: (data) => {
+        return axios.post("/api/v0/auth/login", data, self.headers);
+    },
+}
